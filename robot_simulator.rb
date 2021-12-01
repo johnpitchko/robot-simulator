3f615527688e02a8b46ebc83aa62dc67119f7a99
John Pitchko <john.pitchko@icloud.com>
2021-11-30 21:28:26 -0700

    Lint file

--- robot_simulator.rb
+++ robot_simulator.rb
@@ -1,10 +1,8 @@
-=begin
-Write your code for the 'Robot Simulator' exercise in this file. Make the tests in
-`robot_simulator_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/robot-simulator` directory.
-=end
+# Write your code for the 'Robot Simulator' exercise in this file. Make the tests in
+# `robot_simulator_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/robot-simulator` directory.
 
 # frozen_string_literal: true
 
@@ -13,7 +11,7 @@ require 'byebug'
 require_relative 'robot'
 
 class Simulator
-  INSTRUCTIONS = {'L' => :turn_left, 'R' => :turn_right, 'A' => :advance}.freeze
+  INSTRUCTIONS = { 'L' => :turn_left, 'R' => :turn_right, 'A' => :advance }.freeze
 
   # Translate an instruction string into the instruction symbols that the robot understands
   def instructions(instruction_string)
@@ -27,7 +25,7 @@ class Simulator
   end
 
   # Send an entire string of commands to a robot
-   def evaluate(robot, instruction_string)
-     instructions(instruction_string).each{ |ins| robot.send(ins) }
-   end
+  def evaluate(robot, instruction_string)
+    instructions(instruction_string).each { |ins| robot.send(ins) }
+  end
 end
